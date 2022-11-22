91288eba5ad9e6cd9168d5e96881f6df24786b8a
Lego79 <kentcandoit89@gmail.com>
2022-11-22 18:01:15 +0900

    aws 스프링 이동욱님 책 실습

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -3,8 +3,8 @@ package com.jojoldu.book.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
+@Getter //모든 필드의 get 메소드 생성
+@RequiredArgsConstructor //final 필드가 포함된 생성자만 생성
 public class HelloResponseDto {
 
     private final String name;
